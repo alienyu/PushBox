7a3850d3c4e014b7e5d20f716eb7755ffe41d22d
alienyu <alienyu@foxmail.com>
2012-02-22 09:50:01 +0800

    2-22 v1

--- app/assets/javascripts/show/ruly.js
+++ app/assets/javascripts/show/ruly.js
@@ -1,5 +1,5 @@
 pbMap=[
-    { wall:'234,24,24567,0127,0567,01235,35,345'
+    { wall:'234,24,24567,0127,0567,01235,35,345',               ground:',3,3'
         ,boy:[[4,4]],box:[[3,3],[3,5],[4,3],[5,4]],             target:[[4,1],[1,3],[3,6],[6,4]]
     }
     ,{ wall:'01234,04,04678,0468,0124568,128,158,15678,12345'
@@ -66,7 +66,7 @@ pushBox={
                 }
             }
         };
-        this.wall=[];this.boy=[];this.box=[];this.target=[];
+        this.wall=[];this.ground=[];this.boy=[];this.box=[];this.target=[];
         /*
          将每一关wall的string表示法转换成数组表示
          */
@@ -74,13 +74,18 @@ pushBox={
         for (var i=0,tmp=[];i<wall.length;i++ )
             for (var j=0;j<wall[i].length;j++ )
                 tmp.push([i,parseInt('0x'+wall[i].charAt(j))]);
+        var ground=pbMap[this.ini.level].ground.split(',');
+        for (var i=0,gd=[];i<ground.length;i++ )
+            for (var j=0;j<ground[i].length;j++ )
+                gd.push([i,parseInt('0x'+ground[i].charAt(j))]);
         this.setScene(tmp,'wall',this.wall);
+        this.setScene(gd,'ground',this.ground);
         this.setScene(pbMap[this.ini.level].target,'target',this.target);
         this.setScene(pbMap[this.ini.level].box,'box',this.box);
         this.setScene(pbMap[this.ini.level].boy,'boy',this.boy);
     }
     /*
-     生成基础表格，先列后行
+     生成15*15的基础表格，先列后行
      */
     ,setMap:function(win,x,y){
         for (var i=0, html=[];i<y;i++ ) {
